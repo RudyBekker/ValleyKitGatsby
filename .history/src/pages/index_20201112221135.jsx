4ee8a9ef45b4f03d32d0693e468729ffb2eb08bd
OptimizedGroup <rudy@optimizedgroup.io>
2020-11-12 22:50:48 +0100

    1st commit

---
+++ .history/src/pages/index_20201112221135.jsx
@@ -0,0 +1,46 @@
+import React from "react"
+import { graphql } from "gatsby"
+import { Cards, Hero, SiteMetadata } from "../components"
+import { Layout } from "../layouts/Layout"
+
+export default ({ data }) => {
+  return (
+    <Layout>
+      <SiteMetadata
+        title="Valley Kit Homes"
+        description="WELCOME TO VALLEY KIT HOMES, AUSTRALIA’S PREFERRED KIT HOME SPECIALISTS"
+        image={data.hero.url}
+      />
+
+      <Hero
+        image={data.hero}
+        tag="#travel"
+        title="Valley Kit Homes"
+        description="WELCOME TO VALLEY KIT HOMES, AUSTRALIA’S PREFERRED KIT HOME SPECIALISTS"
+      />
+
+      <Cards nodes={data.items.nodes} />
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query IndexQuery($tableName: String!) {
+    hero: file(relativePath: { eq: "hero-travel.jpg" }) {
+      ...HeroImageFragment
+    }
+    items: allAirtable(filter: { table: { eq: $tableName } }) {
+      nodes {
+        data {
+          country
+          image {
+            ...CardImageFragment
+          }
+          name
+          slug
+          summary
+        }
+      }
+    }
+  }
+`
